cae9a26ccdb75a673f208ed82cb9347555da11cc
Kishore Rajkumar <kishore.rajkumar@hotmail.com>
2025-09-11 00:57:27 +0530

    conditional check for null or empty endpoint added for dynamodb client builder

--- src/main/java/com/kishore/taskmanager/config/AwsConfig.java
+++ src/main/java/com/kishore/taskmanager/config/AwsConfig.java
@@ -7,18 +7,23 @@ import org.springframework.context.annotation.Configuration;
 
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClientBuilder;
 
 @Configuration
 public class AwsConfig {
-    @Bean
-    public DynamoDbClient dynamoDbClient(DynamoProperties props) {
-    	
-    	System.out.println("🔧 DynamoDB Endpoint: " + props.getEndpoint());
-        System.out.println("🔧 DynamoDB Region: " + props.getRegion());
+	@Bean
+	public DynamoDbClient dynamoDbClient(DynamoProperties props) {
 
-        return DynamoDbClient.builder()
-            .endpointOverride(URI.create(props.getEndpoint()))
-            .region(Region.of(props.getRegion())) // ✅ FIXED
-            .build();
-    }
+		System.out.println("🔧 DynamoDB Endpoint: " + props.getEndpoint());
+		System.out.println("🔧 DynamoDB Region: " + props.getRegion());
+
+		DynamoDbClientBuilder builder = DynamoDbClient.builder()
+				.region(Region.of(props.getRegion()));
+
+		if (props.getEndpoint() != null && !props.getEndpoint().isBlank()) {
+			builder.endpointOverride(URI.create(props.getEndpoint()));
+		}
+
+		return builder.build();
+	}
 }
