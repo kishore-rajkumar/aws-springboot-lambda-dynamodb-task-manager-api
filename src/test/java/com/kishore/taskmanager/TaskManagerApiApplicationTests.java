979e4be2de20497403e508330c4373b1505beaa5
Kishore Rajkumar <kishore.rajkumar@hotmail.com>
2025-09-15 20:39:15 +0530

    fixed bug in testing with profile:test

--- src/test/java/com/kishore/taskmanager/TaskManagerApiApplicationTests.java
+++ src/test/java/com/kishore/taskmanager/TaskManagerApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.kishore.taskmanager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class TaskManagerApiApplicationTests {
 
 	@Test
